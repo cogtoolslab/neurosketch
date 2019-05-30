e2aa917c6e38c5e1a8230dcc99a0d537432d9637
JDWammes <jdwammes@gmail.com>
2019-05-30 18:31:23 -0400

    allow preprocessing of feat output, generation of recog features

--- ipynb/jdwammes/01_output_to_highres_standard.sh
+++ ipynb/jdwammes/01_output_to_highres_standard.sh
@@ -14,12 +14,12 @@ pushd $SLURM_SUBMIT_DIR > /dev/null   # move into the subject's directory, quiet
 CURR_DIR=$SLURM_SUBMIT_DIR
 FL_DIR=$CURR_DIR/analysis/firstlevel
 source $CURR_DIR/globals.sh # load subject-wide settings
-STANDARD=$CURR_DIR/standard/MNI152_T1_2mm_brain
+STANDARD=$CURR_DIR/../../standard/MNI152_T1_2mm_brain
 
 ### Which runs are being processed? Delineate below in run list. 
 
-runList="preproc_recognition_run_1.feat preproc_recognition_run_2.feat preproc_recognition_run_3.feat \
-         preproc_recognition_run_4.feat preproc_recognition_run_5.feat preproc_recognition_run_6.feat \
+runList="glm4_recognition_run_1 glm4_recognition_run_2 glm4_recognition_run_3 \
+         glm4_recognition_run_4 glm4_recognition_run_5 glm4_recognition_run_6 \
          draw_run_1, draw_run_2, draw_run_3, draw_run_4"
 copeList="1 2 3 4"
 
@@ -33,9 +33,9 @@ do
   ff=$FL_DIR/${r}.feat/reg/example_func2highres.nii.gz
   outff=$FL_DIR/${r}.feat/reg/func2highres2mm.nii.gz
   MAT=$FL_DIR/${r}.feat/reg/example_func2highres.mat
-  STAN=$FL_DIR/${r}.feat/reg/example_func2standard.mat
-  #flirt -ref $HIRES  -in $FF -applyisoxfm 2 -out $OUTFF -interp spline
-  #flirt -ref $HIRES  -in $ff -applyisoxfm 2 -out -init $MAT $outff -interp spline
+  STAN=$FL_DIR/${r}.feat/reg/func2standard.mat
+  flirt -ref $HIRES  -in $FF -applyisoxfm 2 -out $OUTFF -interp spline
+  flirt -ref $HIRES  -in $ff -applyisoxfm 2 -out $outff -init $MAT -interp spline
   
   if [[ ${r:0:4} != "draw" ]]; 
   then
@@ -44,8 +44,8 @@ do
       INCOPE=$FL_DIR/${r}.feat/stats/cope${cope}.nii.gz
       OUTCOPE=$FL_DIR/${r}.feat/stats/cope${cope}_HIRES.nii.gz
       STANCOPE=$FL_DIR/${r}.feat/stats/cope${cope}_STAN.nii.gz
-      #flirt -ref $HIRES  -in $INCOPE -applyisoxfm 2 -out $OUTCOPE -init $MAT -interp spline
-      #flirt -ref $STANDARD  -in $INCOPE -applyisoxfm 2 -out $STANCOPE -init $STAN -interp spline
+      flirt -ref $HIRES  -in $INCOPE -applyisoxfm 2 -out $OUTCOPE -init $MAT -interp spline
+      flirt -ref $STANDARD  -in $INCOPE -applyisoxfm 2 -out $STANCOPE -init $STAN -interp spline
     done
   fi
   echo "run ${r} in T1 space -- $(date)"
